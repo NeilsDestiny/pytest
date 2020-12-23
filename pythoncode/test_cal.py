b4e89109e1261810945ad2a174e77f119511b104
Destiny <weiyph@126.com>
2020-12-23 11:06:23 +0800

    增加乘法和除法的用例

--- pythoncode/test_cal.py
+++ pythoncode/test_cal.py
@@ -6,6 +6,7 @@ from pythoncode.calculator import Calculator
 with open('./data.yml') as f:
     data = yaml.safe_load(f)
 
+
 class TestCal:
     # cal = Calculator()
     def setup_class(self):
@@ -22,6 +23,16 @@ class TestCal:
         assert res == exp
 
     @pytest.mark.parametrize('a,b,exp', data['sub'])
-    def test_sub(self,a,b,exp):
-        res =  self.cal.sub(a,b)
+    def test_sub(self, a, b, exp):
+        res = self.cal.sub(a, b)
+        assert res == exp
+
+    @pytest.mark.parametrize('a,b,exp', data['mul'])
+    def test_mul(self, a, b, exp):
+        res = self.cal.mul(a, b)
+        assert res == exp
+
+    @pytest.mark.parametrize('a,b,exp', data['div'])
+    def test_div(self, a, b, exp):
+        res = self.cal.div(a, b)
         assert res == exp
