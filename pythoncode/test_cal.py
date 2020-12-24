e4a76d610dc39a64415c8883fd219900402910c8
Destiny <weiyph@126.com>
2020-12-24 12:47:42 +0800

    添加每条用例的名称

--- pythoncode/test_cal.py
+++ pythoncode/test_cal.py
@@ -1,38 +1,41 @@
 import pytest
 import yaml
-
 from pythoncode.calculator import Calculator
 
 with open('./data.yml') as f:
-    data = yaml.safe_load(f)
+    file = yaml.safe_load(f)
+    # 获取用例参数
+    datas = file['datas']
+    # 获取用例标题
+    ids = file['ids']
 
 
 class TestCal:
-    # cal = Calculator()
+
     def setup_class(self):
         print("\n开始计算")
+        # 实例化计算器的类
         self.cal = Calculator()
 
     def teardown_class(self):
         print("\n结束计算")
 
-    @pytest.mark.parametrize('a,b,exp', data['add'])
+    @pytest.mark.parametrize('a,b,exp', datas['add'], ids=ids['add'])
     def test_add(self, a, b, exp):
         res = self.cal.add(a, b)
-        # print(res)
         assert res == exp
 
-    @pytest.mark.parametrize('a,b,exp', data['sub'])
+    @pytest.mark.parametrize('a,b,exp', datas['sub'], ids=ids['sub'])
     def test_sub(self, a, b, exp):
         res = self.cal.sub(a, b)
         assert res == exp
 
-    @pytest.mark.parametrize('a,b,exp', data['mul'])
+    @pytest.mark.parametrize('a,b,exp', datas['mul'], ids=ids['mul'])
     def test_mul(self, a, b, exp):
         res = self.cal.mul(a, b)
         assert res == exp
 
-    @pytest.mark.parametrize('a,b,exp', data['div'])
+    @pytest.mark.parametrize('a,b,exp', datas['div'], ids=ids['div'])
     def test_div(self, a, b, exp):
         res = self.cal.div(a, b)
         assert res == exp
